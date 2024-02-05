5c3e9356880825fef576d72cf2379438450631b0
Marvin <okongomarvin971@gmail.com>
2024-02-05 14:20:34 +0300

    Completed Assignment

--- bank-of-flariron/src/TransactionTable.js
+++ bank-of-flariron/src/TransactionTable.js
@@ -6,18 +6,22 @@ const TransactionTable = ({ transactions, onDelete }) => {
     <table>
       <thead>
         <tr>
+          <th>No.</th>
           <th>Description</th>
           <th>Amount ($)</th>
           <th>Category</th>
+          <th>Date</th>
           <th>Action</th>
         </tr>
       </thead>
       <tbody>
         {transactions.map((transaction) => (
           <tr key={transaction.id}>
+            <td>{transaction.id}</td>
             <td>{transaction.description}</td>
             <td>{transaction.amount}</td>
             <td>{transaction.category}</td>
+            <td>{transaction.date}</td>
             <td>
               <button onClick={() => onDelete(transaction.id)}>Delete</button>
             </td>
